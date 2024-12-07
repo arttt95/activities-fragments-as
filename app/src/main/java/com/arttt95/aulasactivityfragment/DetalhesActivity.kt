55fac8e3549e522a7b073492389a413cc0158091
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-07 16:52:36 -0300

    07DEZ24 - Aula 101 AS - Navegação entre Activities.

---
+++ app/src/main/java/com/arttt95/aulasactivityfragment/DetalhesActivity.kt
@@ -0,0 +1,33 @@
+package com.arttt95.aulasactivityfragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetalhesActivity : AppCompatActivity() {
+
+    private lateinit var btnFechar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalhes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnFechar = findViewById(R.id.btn_fechar)
+        btnFechar.setOnClickListener {
+
+            finish()
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/arttt95/aulasactivityfragment/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.arttt95.aulasactivityfragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnAbrir: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnAbrir = findViewById(R.id.btn_abrir)
+        btnAbrir.setOnClickListener {
+
+            var intent = Intent(this, DetalhesActivity::class.java)
+
+            startActivity(intent)
+
+        }
+
+    }
+
+
+
+}
